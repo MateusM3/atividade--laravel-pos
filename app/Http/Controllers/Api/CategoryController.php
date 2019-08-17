20b74c5586238837c186a00c3f75e9baa5ecd94b
Mateus Monteiro <mateusmonteirom3@gmail.com>
2019-08-17 09:58:22 -0300

    CRUD for category.

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return Category::latest()->get();
+    }
+
+    public function store(Request $request, Category $category)
+    {
+        $category::create(array(
+            'name' => $request->name,
+            'slug' => str_slug($request->name),
+        ));
+
+        return response('created', Response::HTTP_CREATED);
+    }
+
+    public function show(Category $category)
+    {
+        return $category;
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $category->update(
+            [
+                'name' => $request->name,
+                'slug' => str_slug($request->name)
+            ]
+        );
+
+        return response('Updated', Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return redirect(null, Response::HTTP_NO_CONTENT);
+    }
+}
